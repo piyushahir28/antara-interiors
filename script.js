5e1d43760b0f21953a1b494594bfdd799825cd20
Piyush Ahir <egpiyushahir@gmail.com>
2023-04-07 17:02:36 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,21 @@
+firstText = "Interiors";
+secondText = "3D Art";
+thirdText = "HomeDecor"
+intervalTime = 600;
+window.load = displayText();
+function displayText() {
+    // display first text
+    document.querySelector('#dynamicContent').innerText = firstText;
+    // display second text
+    setTimeout(() => {
+        document.querySelector('#dynamicContent').innerText = secondText;
+    }, intervalTime * 3);
+    // display third text
+    setTimeout(() => {
+        document.querySelector('#dynamicContent').innerText = thirdText;
+    }, intervalTime * 5);
+}
+
+setInterval(() => {
+    displayText();
+}, intervalTime * 7);
